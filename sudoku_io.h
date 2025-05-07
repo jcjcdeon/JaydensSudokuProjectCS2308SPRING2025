0583ba0f41610e39430739398cc37ac98b7a3212
Jayden Crawford <dma186@txstate.edu>
2025-05-07 10:15:16 -0500

    sudoku_io.h

--- sudoku_io.h
+++ sudoku_io.h
@@ -3,8 +3,21 @@
 
 #include <string>
 
-void printBoard(int board[9][9]);
-bool loadBoardFromFile(const std::string& filename, int board[9][9]);
-bool saveBoardToFile(const std::string& filename, int board[9][9]);
+/**
+ * Reads a Sudoku board from a file.
+ *
+ * @param filename The name of the file to read from.
+ * @param board The 9x9 Sudoku grid to fill.
+ * @return true if the board was successfully loaded, false otherwise.
+ */
+bool readBoardFromFile(const std::string& filename, int board[9][9]);
+
+/**
+ * Writes a Sudoku board to a file.
+ *
+ * @param filename The name of the file to write to.
+ * @param board The 9x9 Sudoku grid to write.
+ */
+void writeBoardToFile(const std::string& filename, int board[9][9]);
 
 #endif // SUDOKU_IO_H
