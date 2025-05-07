065b51cd3b08a5dc3ea449cea00b8f783fe1f1f0
Jayden Crawford <dma186@txstate.edu>
2025-05-07 10:14:39 -0500

    sudoku.h

--- sudoku.h
+++ sudoku.h
@@ -1,16 +1,10 @@
 #ifndef SUDOKU_H
 #define SUDOKU_H
 
-// Function to solve Sudoku using basic backtracking
+// Declaration of the Sudoku solver function
 bool solveBasic(int board[9][9]);
 
-// Function to solve Sudoku using a more optimized method (if implemented)
-bool solveEfficient(int board[9][9]);
+// Declaration for the function that prints the Sudoku board
+void printBoard(const int board[9][9]);
 
-// Check if a number can be placed at (row, col)
-bool isValid(int board[9][9], int row, int col, int num);
-
-// Validate if a completed board is a valid Sudoku solution
-bool isValidBoard(int board[9][9]);
-
-#endif // SUDOKU_H
+#endif
