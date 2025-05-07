8234932b7e4104bc73e2d6b7b774f31c41609ba0
Jayden Crawford <dma186@txstate.edu>
2025-05-07 10:13:43 -0500

    generator.h

--- generator.h
+++ generator.h
@@ -1,12 +1,30 @@
 #ifndef GENERATOR_H
 #define GENERATOR_H
 
-// Function declarations for generating a Sudoku puzzle
-void generatePuzzle(int board[9][9]);
-void shuffleBoard(int board[9][9]);
+/**
+ * Initializes a 9x9 Sudoku board with zeros.
+ * @param board The 9x9 Sudoku grid to initialize
+ */
 void initializeBoard(int board[9][9]);
-void fillDiagonal(int board[9][9]);
-void fillBox(int board[9][9], int row, int col);
-void removeDigits(int board[9][9], int count);
+
+/**
+ * Generates a fully solved, valid Sudoku board.
+ * @param board The 9x9 grid to fill with a complete solution
+ */
+void generateCompleteBoard(int board[9][9]);
+
+/**
+ * Generates a Sudoku puzzle with a specified number of clues.
+ * @param board The 9x9 grid to fill with a puzzle
+ * @param clues The number of filled cells to retain (difficulty)
+ */
+void generatePuzzle(int board[9][9], int clues);
+
+/**
+ * Randomly shuffles rows and columns within 3x3 blocks.
+ * Used to obscure the pattern of a solved board.
+ * @param board The 9x9 grid to shuffle
+ */
+void shuffleBoard(int board[9][9]);
 
 #endif // GENERATOR_H
